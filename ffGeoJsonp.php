1b615c63b3cbc339982e57982c966d8a859184cc
andibraeu <ab@andi95.de>
2014-12-27 10:43:07 +0100

    jsonp: added security checks

--- ffGeoJsonp.php
+++ ffGeoJsonp.php
@@ -1,8 +1,36 @@
 <?php header('content-type: application/json; charset=utf-8');
 
-$url ="http://fossasia.net/map/ffGeoJson.json";
+function is_valid_callback($subject)
+{
+    $identifier_syntax
+      = '/^[$_\p{L}][$_\p{L}\p{Mn}\p{Mc}\p{Nd}\p{Pc}\x{200C}\x{200D}]*+$/u';
+
+    $reserved_words = array('break', 'do', 'instanceof', 'typeof', 'case',
+      'else', 'new', 'var', 'catch', 'finally', 'return', 'void', 'continue', 
+      'for', 'switch', 'while', 'debugger', 'function', 'this', 'with', 
+      'default', 'if', 'throw', 'delete', 'in', 'try', 'class', 'enum', 
+      'extends', 'super', 'const', 'export', 'import', 'implements', 'let', 
+      'private', 'public', 'yield', 'interface', 'package', 'protected', 
+      'static', 'null', 'true', 'false');
+
+    return preg_match($identifier_syntax, $subject)
+        && ! in_array(mb_strtolower($subject, 'UTF-8'), $reserved_words);
+}
+
+
+$url ="ffGeoJson.json";
 $data = file_get_contents($url);
 $json = json_encode($data);
-$data = json_decode($json);
+$json = json_decode($json);
 
-echo $_GET['callback'] . '('.$data.')';
+# JSON if no callback
+if( ! isset($_GET['callback']) )
+    exit($json);
+
+# JSONP if valid callback
+if(is_valid_callback($_GET['callback']))
+    exit("{$_GET['callback']}($json)");
+
+# Otherwise, bad request
+header('status: 400 Bad Request', true, 400);
+
